ce46681d21fdeb2b05be6c656f928faad6e87d79
Karim Sonbol <karim.sonbol279@gmail.com>
2014-10-01 10:29:20 +0200

    added configs for heroku

--- sf_guide/settings/production.py
+++ sf_guide/settings/production.py
@@ -1,13 +1,17 @@
 from common import *
 from getenv import env
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+
+DATABASES['default'] =  dj_database_url.config()
 SECRET_KEY = env('SECRET_KEY')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASSWORD')
-    }
-}
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'static'
+
